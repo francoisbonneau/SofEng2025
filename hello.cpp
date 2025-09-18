b8ecf09327a2ce41b6add503b309ba1ea996f884
Francois Bonneau <francois.bonneau@geode-solutions.com>
2025-09-18 11:41:49 +0200

    config file

--- hello.cpp
+++ hello.cpp
@@ -1,10 +1,20 @@
 #include <iostream>
+#include<HelloProjConfig.h>
 
 void hello_world() {
-      std::cout << "Hello World!\n";
+    std::cout << "Hello World!\n";
+}
+
+void print_version() {
+    std::cout << "project version is "
+        << HelloProj_VERSION_MAJOR
+        << "."
+        << HelloProj_VERSION_MINOR
+        << std::endl;
 }
 
 int main()
 {
-      hello_world();
+    hello_world();
+    print_version();
 }
